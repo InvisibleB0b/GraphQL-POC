f23f55dedac28c1f4c40a0e29285192b62343589
InvisibleB0b <theodor.per@gmail.com>
2021-12-06 18:35:55 +0100

    Add project files.

---
+++ Queries/TestQuery.cs
@@ -0,0 +1,21 @@
+﻿using GraphQL_POC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQL_POC.Queries
+{
+    public class TestQuery
+    {
+        public Book GetBook() =>
+            new Book
+            {
+                Title = "C# in depth.",
+                Author = new Author
+                {
+                    Name = "Jon Skeet"
+                }
+            };
+    }
+}
